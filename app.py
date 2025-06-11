2875ed1c2c1f82e547122857a4dc52534c5f395e
xPaul74 <57302513+xPaul74@users.noreply.github.com>
2025-06-11 16:18:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -122,4 +122,4 @@ import os
 if __name__ == "__main__":
     print("→ Pornim aplicația Flask pe Azure...")
     port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
